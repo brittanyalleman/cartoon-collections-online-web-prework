4f19ecff9235c71544aedc0aa99dd6e4876e9b24
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-09 21:47:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(names)
+ names.each_with_index do |name,i|
+puts ("#{i+1}. #{name}")
+ end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |word|
+    "#{word.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
