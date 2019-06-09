22cb2ba8ed8431d72f72d137d67ca9958860e2f1
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-09 22:37:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,13 @@ end
 
 def long_planeteer_calls(array)
     array.join  # want to turn array into string
-      array.count      #   want to count letters in words
-        word=array.count
-               if 4 < word
-               return false
-             else
-               return true
-end
+    array.count      #   want to count letters in words
+    word=array.count
+     if 4 < word
+     return false
+     else
+     return true
+     end
 end
 def find_the_cheese# code an argument here
   # the array below is here to help
